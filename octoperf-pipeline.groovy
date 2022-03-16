fcaa8575c43514241ae30b09916236896030966e
RijiyaRaphael <46004271+RijiyaRaphael@users.noreply.github.com>
2022-03-16 13:15:47 +0530

    Add files via upload

---
+++ octoperf-pipeline.groovy
@@ -0,0 +1,32 @@
+#!/usr/bin/env groovy
+node {
+
+    stage('Initialise') {
+        /* Checkout the scripts */
+        checkout scm: [
+                $class: 'GitSCM',
+                userRemoteConfigs: [
+                        [
+                                url: "https://github.com/RijiyaRaphael/jenkins_Integration.git",
+                                credentialsId: "rijiya"
+                        ]
+                ],
+                branches: [[name: "main"]]
+        ], poll: false
+    }
+
+    stage('Complete any setup steps') {
+        echo "Complete set-up steps"
+        //echo "${octoperf_test_value}"
+    }
+
+    stage('Execute Performance Tests') {
+        dir("${WORKSPACE}/Scripts") {
+            bat "F:\apache-jmeter-5.3\apache-jmeter-5.3\binjmeter.bat -n -t F:\apache-jmeter-5.3\apache-jmeter-5.3\bindemojmeter.jmx -l F:\apache-jmeter-5.3\apache-jmeter-5.3\bindemo4.jtl"
+        }
+    }
+
+    stage('Analyse Results') {
+        echo "Analyse results"
+    }
+}
