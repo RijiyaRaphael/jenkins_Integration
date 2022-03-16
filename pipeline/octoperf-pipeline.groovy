1601b27740008ce32478368ad9025596cb25be50
RijiyaRaphael <46004271+RijiyaRaphael@users.noreply.github.com>
2022-03-16 15:03:16 +0530

    Update octoperf-pipeline.groovy

--- pipeline/octoperf-pipeline.groovy
+++ pipeline/octoperf-pipeline.groovy
@@ -28,12 +28,5 @@ node {
 
     stage('Analyse Results') {
         echo "Analyse results"
-        publishHTML(target : [allowMissing: false,
-                     alwaysLinkToLastBuild: true,
-                     keepAll: true, 
-                     reportDir: 'pipeline',
-                     reportFiles: 'index.html',
-                     reportName: 'report',
-                     reportTitles: 'octoperf Report'])
     }
 }
