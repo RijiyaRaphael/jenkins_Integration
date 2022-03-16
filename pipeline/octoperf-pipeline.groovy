dc34a4db3c5ccd988ca40e24a9dd3277033e8445
RijiyaRaphael <46004271+RijiyaRaphael@users.noreply.github.com>
2022-03-16 13:37:57 +0530

    Update octoperf-pipeline.groovy

--- pipeline/octoperf-pipeline.groovy
+++ pipeline/octoperf-pipeline.groovy
@@ -22,7 +22,7 @@ node {
 
     stage('Execute Performance Tests') {
         dir("${WORKSPACE}") {
-            bat "F:\apache-jmeter-5.3\apache-jmeter-5.3\bin -n -t demojmeter.jmx -l demo4.jtl"
+            bat "c:\apache-jmeter\apache-jmeter\bin\jmeter.bat -n -t demojmeter.jmx -l demo4.jtl"
         }
     }
 
